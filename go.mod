be5349f982e6a09d10520e4afcb6e765cb945c46
Leonid Belyaev <lbeliaev@gmail.com>
2023-10-15 20:27:43 -0700

    threads as param

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module xenminer-go
 go 1.21.1
 
 require (
+	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
+	github.com/tvdburgt/go-argon2 v0.0.0-20181109175329-49d0f0e5973c // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
--- miner.go
+++ miner.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
+	// "crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"golang.org/x/crypto/argon2"
+	"github.com/minio/sha256-simd"
+	// "golang.org/x/crypto/argon2"
+	"github.com/tvdburgt/go-argon2"
+	"io"
 	"log"
 	rnd "math/rand"
+	"net/http"
 	"runtime"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -23,25 +29,32 @@ type params struct {
 	keyLength   uint32
 }
 
-func generateArgonHash(password string, p *params) (string, error) {
+func generateArgonHash(password string, salt []byte, p *params) (string, error) {
 	// Generate a cryptographically secure random salt.
-	salt, err := generateRandomBytes(p.saltLength)
-	if err != nil {
-		log.Println("Error generating random ", err)
-		return "", err
-	}
 
 	// Pass the plaintext password, salt and parameters to the argon2.IDKey
 	// function. This will generate a hash of the password using the Argon2id
 	// variant.
-	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	// hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	ctx := &argon2.Context{
+		Iterations:  1,
+		Memory:      int(p.memory),
+		Parallelism: 1,
+		HashLen:     int(p.keyLength),
+		Mode:        argon2.ModeArgon2id,
+		Version:     argon2.Version13,
+	}
+	hash, err := argon2.Hash(ctx, []byte(password), salt)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Base64 encode the salt and hashed password.
 	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
 	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
 
 	// Return a string using the standard encoded hash representation.
-	encodedHash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
+	encodedHash := fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version13, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
 
 	return encodedHash, nil
 }
@@ -69,13 +82,45 @@ func generateRandomSHA256(maxLength int) string {
 	return fmt.Sprintf("%x", sha256Hash)
 }
 
+func getDifficulty(url string) uint {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		return 0
+	} else {
+		reqBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("error decoding http request: %s\n", err)
+		}
+		var blocks XenBlocks
+		err = json.Unmarshal(reqBody, &blocks)
+		if err != nil {
+			fmt.Printf("error decoding json: %s\n", err)
+		}
+		i, err := strconv.Atoi(blocks.Difficulty)
+		if err != nil {
+			fmt.Printf("error decoding json: %s\n", err)
+		}
+		return uint(i)
+	}
+}
+
+type XenBlocks struct {
+	Difficulty string `json:"difficulty"`
+}
+
 func main() {
 
 	threads := flag.Uint64("threads", uint64(runtime.NumCPU()), "no of threads")
 	flag.Parse()
 
+	log.Printf("Using %d threads\n", *threads)
+
+	diff := getDifficulty("http://xenblocks.io/difficulty")
+	log.Printf("Diff: %d\n", diff)
+
 	p := &params{
-		memory:      64 * 1024,
+		memory:      uint32(diff),
 		iterations:  1,
 		parallelism: 1,
 		saltLength:  16,
@@ -85,11 +130,16 @@ func main() {
 	var count int
 	interval := 1 * time.Second // Measurement interval
 
+	salt, err := generateRandomBytes(p.saltLength)
+	if err != nil {
+		log.Println("Error generating random ", err)
+	}
+
 	go func() {
 		for {
 			<-time.After(interval) // Wait for the interval to elapse
 			rate := float64(count) / interval.Seconds()
-			fmt.Printf("Rate per second: %.2f\n", rate)
+			fmt.Printf("\rRate per second: %.2f", rate)
 			count = 0 // Reset the count for the next interval
 		}
 	}()
@@ -97,15 +147,32 @@ func main() {
 	mine := func() {
 		for {
 			hash := generateRandomSHA256(128)
-			hashedData, _ := generateArgonHash(hash, p)
-			if strings.Contains(hashedData, "XEN11") {
+			hashedData, _ := generateArgonHash(hash, salt, p)
+			if strings.Contains(hashedData[len(hashedData)-87:], "XEN11") {
 				log.Println(hashedData)
 			}
 			count += 1
 		}
 	}
 
-	log.Printf("Using %d threads\n", *threads)
+	getDiff := func() {
+		t := time.NewTicker(60 * time.Second)
+		defer t.Stop()
+		quit := make(chan struct{})
+
+		for {
+			select {
+			case <-t.C:
+				diff = getDifficulty("http://xenblocks.io/difficulty")
+				log.Printf("Diff: %d\n", diff)
+
+			case <-quit:
+				t.Stop()
+				return
+			}
+		}
+	}
+
 	var wg sync.WaitGroup
 	var i uint64
 
@@ -113,6 +180,9 @@ func main() {
 		go mine()
 		wg.Add(1)
 	}
+
+	go getDiff()
+	wg.Add(1)
 
 	wg.Wait()
 
